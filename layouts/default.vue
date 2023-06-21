deef5ec7502a7e394325e5c20f56f971305d7650
midlantica <druharper@gmail.com>
2023-06-21 18:03:22 -0500

    save

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,12 @@
   <nav class="flex gap-6 text-3xl">
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="/movies">Movies</NuxtLink>
-    <!-- <NuxtLink to="http://drewharper.com" target="_BLANK">DrewHarper.com</NuxtLink> -->
+    <NuxtLink v-if="!isLoggedIn" to="/login">Login</NuxtLink>
+    <a v-else href="/" @click.prevent="isLoggedIn = false">Logout</a>
   </nav>
   <slot></slot>
 </template>
+
+<script setup>
+  const isLoggedIn = useIsLoggedIn()
+</script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Login</h1>
-    <form>
+    <form @submit.prevent="login">
       <label>
         Username
         <input type="text">
@@ -10,14 +10,21 @@
         Password
         <input type="password">
       </label>
+      <button>Login</button>
     </form>
   </div>
 </template>
 
 <script setup>
+  const isLoggedIn = useIsLoggedIn()
   definePageMeta({
     layout: `plain`
   })
+
+  const login = () => {
+    isLoggedIn.value = true
+    useRouter().push("/")
+  }
 </script>
 
 <style scoped>
